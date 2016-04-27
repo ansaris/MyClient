d7c5b2857802eb9f5c2278eb45db14287f512658
ansaris <salman.ansari.usa@gmail.com>
2016-04-27 15:40:11 -0500

    Add reference to library MyCodeLibrary and test scraping.

--- MyClient/Program.cs
+++ MyClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyCodeLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,11 @@ namespace MyClient
     {
         static void Main(string[] args)
         {
+            Scrape myScrape = new Scrape();
+            //string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+            string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com", @"c:\users\public\testfolder\html2.txt");
+            Console.WriteLine(value);
+            Console.ReadLine();
         }
     }
 }
